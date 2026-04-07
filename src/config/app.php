b518b834eed483a5547042390f2581e07964f813
Erick Coll Rodríguez <erickcollrodriguez@gmail.com>
2026-04-07 19:15:54 +0200

    configura producto 2 para local y techlab

--- src/config/app.php
+++ src/config/app.php
@@ -1,6 +1,12 @@
 <?php
 
+$httpHost = $_SERVER['HTTP_HOST'] ?? '';
+
+$isLocal =
+    strpos($httpHost, 'localhost') !== false ||
+    strpos($httpHost, '127.0.0.1') !== false;
+
 return [
     'name' => 'ReparaYa',
-    'base_url' => '/public'
+    'base_url' => $isLocal ? '/public' : '/~uocx3/Producto2/public'
 ];
